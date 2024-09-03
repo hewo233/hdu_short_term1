8c46635b5939e8822dcd07bbf62befc987fbd66f
hewo <2842137843@qq.com>
2024-09-03 14:48:06 +0800

    data mining

--- src/data_mining/view.py
+++ src/data_mining/view.py
@@ -1,21 +1,46 @@
-
 import folium
 from folium.plugins import MarkerCluster
 
-def visualize_clusters_on_map(data):
-    map = folium.Map(location=[40.7128, -74.0060], zoom_start=12)
-    marker_cluster = MarkerCluster().add_to(map)
+def visualize_start_end_points(data):
+    """
+    Visualize start and end points of bike rides on a map using different colors.
 
-    # 只为聚类中心添加标记
-    print('visualize_clusters_on_map')
+    Args:
+    data (DataFrame): DataFrame with 'start_lat', 'start_lng', 'end_lat', 'end_lng'.
+
+    Returns:
+    Folium Map: A map with start and end points plotted.
+    """
+    # 创建地图，中心位于纽约市
+    map = folium.Map(location=[40.7128, -74.0060], zoom_start=12)
+
+    # 为起点创建一个标记群组
+    start_marker_cluster = MarkerCluster(name='Start Points').add_to(map)
+    # 为终点创建一个标记群组
+    end_marker_cluster = MarkerCluster(name='End Points').add_to(map)
+
+    # 在地图上添加起点标记
     for idx, row in data.iterrows():
         folium.Marker(
-            location=[row['lat'], row['lng']],
-            popup=f'Cluster: {row["cluster"]}',
-            icon=folium.Icon(color='blue', icon='info-sign')
-        ).add_to(marker_cluster)
+            location=[row['start_lat'], row['start_lng']],
+            icon=folium.Icon(color='green', icon='play'),
+            popup='Start Point'
+        ).add_to(start_marker_cluster)
+
+    # 在地图上添加终点标记
+    for idx, row in data.iterrows():
+        folium.Marker(
+            location=[row['end_lat'], row['end_lng']],
+            icon=folium.Icon(color='red', icon='stop'),
+            popup='End Point'
+        ).add_to(end_marker_cluster)
+
+    # 添加图层控制器
+    folium.LayerControl().add_to(map)
 
     return map
 
-
-
+# 使用示例
+# data = pd.read_csv("your_data.csv")
+# map = visualize_start_end_points(data)
+# map.save('NYC_Bike_Rides.html')  # 保存到HTML文件，可以用浏览器打开查看
--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ from load_data import load_csv_data
 from compare import compare_and_visualize_stats
 
 from clust import cluster_geo_locations
-from view import visualize_clusters_on_map
+from view import visualize_start_end_points
 
 data_2020 = load_csv_data('data/final/2020.csv')
 data_2021 = load_csv_data('data/final/2021.csv')
 
 data_2020_clustered, start_centers_2020, end_centers_2020 = cluster_geo_locations(data_2020)
 
-map_2020 = visualize_clusters_on_map(data_2020)
+map_2020 = visualize_start_end_points(data_2020)
 map_2020.save('results/html/2020.html')
